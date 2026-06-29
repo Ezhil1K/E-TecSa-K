e4e6364c3180b69d41eea274e93eb608c70196e7
Ezhil1K <ezhil.1ek@hotmail.com>
2026-06-29 17:50:27 +0530

    Fix: auth.js use ES module syntax to match Vercel runtime

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
-const crypto = require('crypto')
+import crypto from 'crypto'
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
